358fc7222ca3e4bccc95fd4411beda47f2b90c20
cheese98 <cheesej98@gmail.com>
2021-01-17 18:57:09 +0900

    fix test.js

--- commands/test.js
+++ commands/test.js
@@ -3,12 +3,15 @@ var {PythonShell} = require('python-shell')
 module.exports = {
     name: 'test',
     execute(client, message, args) {
+        if (args.length == 0){
+            args.push("--help");  // 주어진 매개변수가 없을 시 도움말로 대체
+        }
         let options = {
             args : args
         }
         PythonShell.run('./maplestory_dpm_calc/test.py', options, function (err, results) {
-            if (err) throw err;
-            message.reply(`${results}`);
+            if (err) message.reply(err.message);
+            else message.reply(`${results}`);
         });
     },
 };
